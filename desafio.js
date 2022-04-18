81ba7d4e31c90d6354b3fc1b0b4fa190b4895ff4
Guido Fernandez <guidofernandez98@gmail.com>
2022-04-18 19:58:10 -0300

    fix

--- desafio.js
+++ desafio.js
@@ -1,3 +1,6 @@
+alert(
+  "Bienvenido a la calculadora de promedio. Ingrese su materia o ingrese ESC si desea salir del programa."
+);
 let materia = prompt("Ingrese la materia:");
 while (materia != "ESC") {
   let nota1 = prompt("Ingrese la primer nota de la materia:");
@@ -5,12 +8,18 @@ while (materia != "ESC") {
   let nota3 = prompt("Ingrese la tercer nota de la materia:");
   let promedio =
     (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3)) / 3;
-  if (promedio > 10 || promedio < 0) {
+  if (promedio > 10 || promedio < 0 || nota1 < 0 || nota2 < 0 || nota3 < 0) {
     alert("Datos ingresados erróneos. Pruebe otra vez.");
-  } else if (promedio >= 6 && promedio <= 10) {
+  } else if (
+    promedio >= 6 &&
+    promedio <= 10 &&
+    nota1 >= 4 &&
+    nota2 >= 4 &&
+    nota3 >= 4
+  ) {
     alert("Su promedio en " + materia + " es de : " + promedio);
     alert("Aprobado.");
-  } else if (promedio < 6 && nota1 > 3 && nota2 > 3 && nota3 > 3) {
+  } else if (promedio < 6 && nota1 >= 4 && nota2 >= 4 && nota3 >= 4) {
     alert("Su promedio en " + materia + " es de : " + promedio);
     alert("Debe recuperar.");
   } else if (promedio < 6 || nota1 <= 3 || nota2 <= 3 || nota3 <= 3) {
